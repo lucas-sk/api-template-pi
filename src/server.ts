d9b9f2c5ee5880ed0f2e0a1cf50abcc032120241
lusk <lucas78654321@outlook.com>
2022-08-20 20:33:08 -0300

    chore: add http-status-codes --save

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
-import express, { response } from 'express';
-import { send } from 'process';
+import express, { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { router } from './routes';
 
 const app = express();
+app.use(express.json());
+app.use(router);
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.listen(5000, () => console.log('listening on port 5000'));
