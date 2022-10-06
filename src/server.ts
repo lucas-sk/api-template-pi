f4238cf55b5614a11dd2ee1bb5b5ea57c091e4c5
lusk <lucas78654321@outlook.com>
2022-10-06 10:25:37 -0300

    feat: add cors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { router } from './routes';
+import cors from "cors";
 
 const PORT = 3000;
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use(router);
 
 app.listen(PORT, () => console.log('server listening on port ' + PORT));
