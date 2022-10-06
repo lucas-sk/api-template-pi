a38898725be7c41c821ea770af66968f2bd75842
lusk <lucas78654321@outlook.com>
2022-10-06 10:27:04 -0300

    refactor: remove check name and pass, because type userCreate

--- src/controller/CreateUserController.ts
+++ src/controller/CreateUserController.ts
@@ -3,32 +3,24 @@ import bcrypt from 'bcrypt';
 import { prismaClient } from '../database/prismaClient';
 import { checkCPF } from '../utils/checkCPF';
 import { checkEmail } from '../utils/checkEmail';
-import { checkNamePass } from '../utils/checkNamePass';
 import { StatusCodes } from 'http-status-codes';
-import { Prisma, PrismaClient } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 
 export class CreateUserController {
   async handle(request: Request, response: Response) {
+    const data: Prisma.UserCreateInput = request.body;
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    if (checkCPF(data.cpf))
+      return response
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: 'cpf não é valido' });
+    if (checkEmail(data.email))
+      return response
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: 'email não é valido' });
+
     try {
-      const data: Prisma.UserCreateInput = request.body;
-
-      if (checkNamePass(data.name, data.password))
-        return response
-          .status(StatusCodes.BAD_REQUEST)
-          .json({ message: 'nome e senha não pode ser vazio' });
-
-      const hashedPassword = await bcrypt.hash(data.password, 10);
-
-      if (checkCPF(data.cpf))
-        return response
-          .status(StatusCodes.BAD_REQUEST)
-          .json({ message: 'cpf não é valido' });
-
-      if (checkEmail(data.email))
-        return response
-          .status(StatusCodes.BAD_REQUEST)
-          .json({ message: 'email não é valido' });
-
       const user = await prismaClient.user.create({
         data: {
           ...data,
@@ -39,7 +31,7 @@ export class CreateUserController {
       return response.status(StatusCodes.CREATED).json(user);
     } catch (error) {
       return response.status(StatusCodes.BAD_GATEWAY).json({
-        message: 'verifique os dados',
+        error,
       });
     }
   }
