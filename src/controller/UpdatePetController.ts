609f4393f19f99b1ac8ec702eae5041193f7228c
lusk <lucas78654321@outlook.com>
2022-10-07 18:14:10 -0300

    refactor: change add type for data request body update pet controller

--- src/controller/UpdatePetController.ts
+++ src/controller/UpdatePetController.ts
@@ -1,31 +1,24 @@
+import { Prisma } from '@prisma/client';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { prismaClient } from '../database/prismaClient';
 
 export class UpdatePetController {
   async handle(request: Request, response: Response) {
+    const { id } = request.params;
+    const data: Prisma.PetUpdateManyMutationInput = request.body;
+    if (data.gender !== 'masc' || data.gender !== 'fem')
+      return response.status(StatusCodes.BAD_REQUEST).json({
+        message: 'O gender só pode ser masc ou fem',
+      });
+
     try {
-      const { id } = request.params;
-      const { name, years, gender, breed, weight, color } = request.body;
-
-      if (gender) {
-        if (gender !== 'masc' || gender !== 'fem')
-          return response.status(StatusCodes.BAD_REQUEST).json({
-            message: 'O gender só pode ser masc ou fem',
-          });
-      }
-
       const pet = await prismaClient.pet.update({
         where: {
           id,
         },
         data: {
-          name,
-          years,
-          gender,
-          breed,
-          weight,
-          color,
+          ...data,
         },
       });
 
