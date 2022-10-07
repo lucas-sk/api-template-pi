318578b9fc49f4f18314b8558483700c42dd60c0
lusk <lucas78654321@outlook.com>
2022-10-07 17:54:58 -0300

    refactor: change check for type

--- src/controller/CreatePetController.ts
+++ src/controller/CreatePetController.ts
@@ -1,38 +1,27 @@
+import { Prisma } from '@prisma/client';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { prismaClient } from '../database/prismaClient';
-import { checkInfoPet } from '../utils/checkInfoPet';
 
 export class CreatePetController {
   async handle(request: Request, response: Response) {
+    const { userId } = request.params;
+    const petInfo: Prisma.PetUncheckedCreateInput = request.body;
+    if (petInfo.gender !== 'masc' && petInfo.gender !== 'fem')
+      return response.status(StatusCodes.BAD_REQUEST).json({
+        message: 'o gender só pode ser masc ou fem',
+      });
+
     try {
-      const { userId } = request.params;
-      const { name, years, gender, breed, weight, color } = request.body;
-
-      if (checkInfoPet(name, years, gender, breed, weight, color))
-        return response.status(StatusCodes.BAD_REQUEST).json({
-          message: 'nenhuma das informações pode ser vazia ou igual 0',
-        });
-
-      if (gender !== 'masc' && gender !== 'fem')
-        return response.status(StatusCodes.BAD_REQUEST).json({
-          message: 'o gender só pode ser masc ou fem',
-        });
-
       const pet = await prismaClient.pet.create({
         data: {
-          name,
-          years,
-          gender,
-          breed,
-          weight,
-          color,
+          ...petInfo,
           userId,
         },
       });
 
       return response.status(StatusCodes.CREATED).json({
-        message: 'Pet adicionado com sucesso',
+        message: pet,
       });
     } catch (error) {
       return response.status(StatusCodes.BAD_GATEWAY).json(error);
